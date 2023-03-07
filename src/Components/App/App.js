54d6b5e2d9e07ea8faef377a8ac8ac6af60190b7
Nikita Lutsenko <nik.luzenko@mail.ru>
2023-03-07 16:17:33 +0600

    added new page Profile Info

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import MainPage from '../MainPage/MainPage';
 import Header from '../Header/Header';
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import ProfileInfo from '../ProfileInfo/ProfileInfo';
 const App = () =>{
   return (
-    <Box className="App">
-      <Header/>
-      <MainPage/>
+    <Box>
+      <Header />
+      {/* <MainPage/> */}
+      <Container maxWidth={'lg'}>
+        <ProfileInfo />
+      </Container>
     </Box>
   );
 }
